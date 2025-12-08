cc4f11011acdc8f07efa880fa990daf992de9a9f
Ariel Plaza <arielplazasalinas@gmail.com>
2025-12-07 23:52:20 -0300

    Creacion de package principal

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.principal.Principal;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
@@ -21,38 +22,42 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        System.out.println("Prueba desde Spring");
-        var consumoAPI = new ConsumoAPI();
-        var json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=9da7ed5e");
-//        var json = consumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
-        System.out.println(json);
 
-        //Serie
-        ConvierteDatos conversor = new ConvierteDatos();
-        var datos = conversor.obtenerDatos(json, DatosSerie.class);
-        System.out.println(datos);
+        Principal principal = new Principal();
+        principal.muestraElMenu();
 
-        //Temporada
-        //Creacion lista con datos temporadas
-        List<DatosTemporadas> temporadas = new ArrayList<>();
+////        System.out.println("Prueba desde Spring");
+//        var consumoAPI = new ConsumoAPI();
+//        var json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=9da7ed5e");
+////        var json = consumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+//        System.out.println(json);
+//
+//        //Serie
+//        ConvierteDatos conversor = new ConvierteDatos();
+//        var datos = conversor.obtenerDatos(json, DatosSerie.class);
+//        System.out.println(datos);
 
-        //bucle se repite hasta el totalTemporadas
-        for (int i = 1; i < datos.totalDeTemporadas(); i++) {
-            //consumo de api, por temporada de cada serie aumenta en cada bucle
-            var jsonTemporadas = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=Young+Sheldon&Season="+i+"&apikey=9da7ed5e");
-            //convierte datos de API a datos de la clase temporada
-            var datosTemporadas = conversor.obtenerDatos(jsonTemporadas,  DatosTemporadas.class);
-            //agrega los datos de la temporada a la lista
-            temporadas.add(datosTemporadas);
-        }
-        //imprime los datos de cada temporada por separado
-        temporadas.forEach(System.out::println);
+//        //Temporada
+//        //Creacion lista con datos temporadas
+//        List<DatosTemporadas> temporadas = new ArrayList<>();
+//
+//        //bucle se repite hasta el totalTemporadas
+//        for (int i = 1; i < datos.totalDeTemporadas(); i++) {
+//            //consumo de api, por temporada de cada serie aumenta en cada bucle
+//            var jsonTemporadas = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=Young+Sheldon&Season="+i+"&apikey=9da7ed5e");
+//            //convierte datos de API a datos de la clase temporada
+//            var datosTemporadas = conversor.obtenerDatos(jsonTemporadas,  DatosTemporadas.class);
+//            //agrega los datos de la temporada a la lista
+//            temporadas.add(datosTemporadas);
+//        }
+//        //imprime los datos de cada temporada por separado
+//        temporadas.forEach(System.out::println);
 
-
-        //Episodios
-        var jsonEpisodio = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=9da7ed5e");
-        var datosEpisodio = conversor.obtenerDatos(jsonEpisodio, DatosEpisodio.class);
-        System.out.println(datosEpisodio);
+//
+//        //Episodios
+//        var jsonEpisodio = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=9da7ed5e");
+//        var datosEpisodio = conversor.obtenerDatos(jsonEpisodio, DatosEpisodio.class);
+//        System.out.println(datosEpisodio);
 
 
 
---
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,40 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private final String URL_BASE = "http://www.omdbapi.com/?t=";
+    private final String  API_KEY = "&apikey=9da7ed5e";
+    private ConvierteDatos conversor =  new ConvierteDatos();
+
+    public void muestraElMenu(){
+        System.out.println("Por favor escribe el nombre de la serie que deseas buscar: ");
+        var nombreSerie =teclado.nextLine();
+
+        var json = consumoAPI.obtenerDatos(URL_BASE + nombreSerie.replace(' ', '+')+ API_KEY);
+        var datos =  conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
+        //Creacion lista con datos temporadas
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+        //bucle se repite hasta el totalTemporadas
+        for (int i = 1; i < datos.totalDeTemporadas(); i++) {
+            //consumo de api, por temporada de cada serie aumenta en cada bucle
+            var jsonTemporadas = consumoAPI.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + "&Season="+ i+ API_KEY );
+            //convierte datos de API a datos de la clase temporada
+            var datosTemporadas = conversor.obtenerDatos(jsonTemporadas,  DatosTemporadas.class);
+            //agrega los datos de la temporada a la lista
+            temporadas.add(datosTemporadas);
+        }
+        //imprime los datos de cada temporada por separado
+        temporadas.forEach(System.out::println);
+    }
+}
